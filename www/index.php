d438fe9611554fe3984942c14a013fdfff80abb6
sergpost3 <sergpost3@mail.ru>
2016-02-27 10:51:51 +0200

    test2

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // comment out the following two lines when deployed to production
-/*defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/vendor/autoload.php');
@@ -9,5 +9,5 @@ require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/config/web.php');
 
-(new yii\web\Application($config))->run();*/
-phpinfo(); 
+(new yii\web\Application($config))->run();
+//phpinfo();
