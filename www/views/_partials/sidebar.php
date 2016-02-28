ea3d912f7bd51a28af7264cd813a15193caa1b56
Nickolas <donald23051995@gmail.com>
2016-02-28 06:44:43 +0200

    28.02.2016 6:44

--- www/views/_partials/sidebar.php
+++ www/views/_partials/sidebar.php
@@ -34,5 +34,43 @@
         <a href=""><img src=".../web/content/Video-Wordpress.png" alt=""></a>
         <a href=""><img src=".../web/content/Video-Wordpress.png" alt=""></a>
         <a href=""><img src=".../web/content/Video-Wordpress.png" alt=""></a>
+        <form action="#" class="container">
+            <h4>Choose audience</h4>
+            <div class="row">
+                <div class="col s7">
+                    <h5>Gender</h5>
+                    <p>
+                      <input name="group1" type="radio" id="test1" />
+                      <label for="test1">Male</label>
+                    </p>
+                    <p>
+                      <input name="group1" type="radio" id="test2" />
+                      <label for="test2">Female</label>
+                    </p>
+                     <p>
+                      <input name="group1" type="radio" id="test3" />
+                      <label for="test3">All</label>
+                    </p>
+                </div>
+                <div class="col s7">
+                    <h5>Social Networks</h5>
+                    <select class="browser-default">
+                        <option value="0" disabled selected>Choose your option</option>
+                        <option value="1">Facebook</option>
+                        <option value="2">VK</option>
+                    </select>
+                </div>
+                <div class="col s7">
+                    <h5>Age</h5>
+                    <p class="range-field">
+                      <input type="number" id="test4" min="0" max="99">
+                      <input type="number" id="test5" min="1" max="100">
+                    </p>
+                </div>
+                <div class="col s7">
+                    <input type="submit" class="btn">
+                </div>
+            </div> 
+        </form>
     </div>
 </div>
